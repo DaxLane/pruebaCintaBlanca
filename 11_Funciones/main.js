1ef921da721052b842cbb87b77b4416e7716e7f0
DaxLan <dactya.silva@me.com>
2020-04-06 21:58:54 -0500

    git actualizado

--- 11_Funciones/main.js
+++ 11_Funciones/main.js
@@ -14,6 +14,10 @@ area1 (5 , 5)
 
 area1 (8 , 4)
 
+
+
+// area de un triangulo
+
 const area2 = (x , h) => {
     console.log(x*h/2);
 }
@@ -30,3 +34,4 @@ area2 (4 , 4);
 // practica 3
 for (let i = 1; i<=100; i++){
         console.log(i);}
+
